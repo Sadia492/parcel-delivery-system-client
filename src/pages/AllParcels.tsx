54d588f7fedcb335924ff1e3d7bf545f9b45ba5c
Sadia492 <sadiaafrin2792@gmail.com>
2025-12-23 21:49:25 +0600

    navbar mega menu fixed

--- src/pages/AllParcels.tsx
+++ src/pages/AllParcels.tsx
@@ -306,7 +306,7 @@ export default function AllParcels() {
                       to={`/parcel/${parcel._id}`}
                       className="flex-1 px-4 py-2.5 bg-primary text-primary-foreground rounded-lg text-sm font-medium hover:bg-primary/90 transition-colors text-center"
                     >
-                      Parcel Details
+                      Parcel Detail
                     </Link>
                   </div>
                 </div>
